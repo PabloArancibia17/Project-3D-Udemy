6337639ed3aa4380bcdc02fb6f600a6c0a302453
PabloArancibia17 <58220331+PabloArancibia17@users.noreply.github.com>
2019-11-28 10:22:20 -0300

    Initial Commit

---
+++ Assets/Manager/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float maxTime = 60f;
+
+    //Variable que indica el tiempo de juego
+    float countdown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Es lo mismo que decir "countdown = countdown - Time.deltaTime"
+        countdown -= Time.deltaTime;
+        Debug.Log("Cuenta atrás: " + countdown);
+
+        if (countdown <= 0)
+        {
+            Debug.Log("Game Over");
+
+            Coin.coinCounter = 0;
+
+            SceneManager.LoadScene("MainScene");
+
+        }
+    }
+}
---
+++ Assets/Prefab/Coin/Coin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    //Se utiliza variable estática dado que se aplica una sola a todos los objetos "Coin"
+    public static int coinCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Código que nos da un contador de monedas, en este caso suma una moneda
+        Coin.coinCounter++;
+        Debug.Log("Se ha creado 1 moneda, en este momento hay " + Coin.coinCounter + " Monedas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //Se resta una moneda al contador
+        Coin.coinCounter--;
+        Destroy(gameObject);
+        Debug.Log("Se ha agarrado una moneda, en este momento hay " + Coin.coinCounter + " monedas");
+
+        if (Coin.coinCounter == 0)
+        {
+            Debug.Log("Se ha terminado el juego");
+
+            //Destruyo el gamemanager para frenar el tiempo
+            GameObject gameManager = GameObject.Find("GameManager");
+            Destroy(gameManager);
+
+            GameObject[] fireworksSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+            foreach(GameObject fireworks in fireworksSystem)
+            {
+                fireworks.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+}
